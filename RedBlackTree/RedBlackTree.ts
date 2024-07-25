fb80fed734413b67d29a8dd597494fba1c7c28b4
riccardobattiato <rbattiato@outlook.it>
2024-07-25 11:13:44 +0200

    feat(rbt): insert and insertfix

--- RedBlackTree/RedBlackTree.ts
+++ RedBlackTree/RedBlackTree.ts
@@ -19,24 +19,100 @@ export class RedBlackTree<T> {
   root: Node<T> | null = null;
 
   insert(value: T): void {
-    this.root = this.insertNode(this.root, value);
-    if (this.root.color === NodeColor.RED) this.root.color = NodeColor.BLACK;
+    const newNode = new Node(value);
+    this.root = this.insertNode(this.root, newNode);
+    this.insertFix(newNode);
+    this.root.color = NodeColor.BLACK; // root must stay black after insertFix's intervention
   }
 
-  private insertNode(node: Node<T> | null, value: T): Node<T> {
-    if (node === null) return new Node(value);
+  private insertNode(node: Node<T> | null, newNode: Node<T>): Node<T> {
+    if (node === null) return newNode; // base case, recursion bounces back from here
 
-    if (value > node.value) node.right = this.insertNode(node.right, value);
-    else if (value < node.value) node.left = this.insertNode(node.left, value);
-
-    if (node.right && node.right.parent === null) node.right.parent = node;
-    if (node.left && node.left.parent === null) node.left.parent = node;
+    // classic binary search tree insertion, plus parent pointer assignment
+    if (newNode.value < node.value) {
+      node.left = this.insertNode(node.left, newNode);
+      node.left.parent = node;
+    } else if (newNode.value > node.value) {
+      node.right = this.insertNode(node.right, newNode);
+      node.right.parent = node;
+    }
 
     return node;
   }
 
+  /* In the end, the whole point of the "rules" is to have an easy-to-follow
+  way to know when it's time to rotate, decreasing the height of the tree - the
+  actual balancing act. However, "rules" allow for a less stricter balancing policy
+  than AVL since many times it will happen that you'll only have to change colors
+  for your Red-Black Tree to stay balanced. */
   private insertFix(node: Node<T>): void {
-    // TODO: Implement the fixup method after insertion to maintain Red-Black properties
+    /*
+    insertFix is always called on the newly inserted node, which is 
+    always red. Moreover, some fixes involve a grandparent node
+    to become red, so we need to loop upwards to ensure there are no red-red
+    nodes chains
+    */
+    while (node.parent?.color === NodeColor.RED) {
+      let parent = node.parent;
+      const grandparent = parent.parent;
+
+      if (!grandparent) break; // this means that parent is root, which constitutes no violation
+
+      // from this moment on we know that if we'll ever need to rotate,
+      // it will be a right rotation
+      if (parent === grandparent.left) {
+        const uncle = grandparent.right;
+
+        /*
+        easy case: if parent level is red for both nodes,
+        we can swap them to red knowing that black-height will
+        increase evenly
+        */
+        if (uncle?.color === NodeColor.RED) {
+          uncle.color = NodeColor.BLACK;
+          parent.color = NodeColor.BLACK;
+          grandparent.color = NodeColor.RED; // we could have introduced a violation here...
+          node = grandparent; // ...so, iterate
+        } else {
+          // harder case: we need to perform one or two rotations, depending on the
+          // alignment of the subtree starting at grandparent
+
+          if (node === parent.right) {
+            /* Remember that in this whole block we know that parent is grandparent's
+          left child, so our rotation will be right. However, we may need a preliminary
+          rotation in case node is misaligned by being right */
+            this.leftRotate(parent);
+            node = parent;
+            parent = node.parent!;
+          }
+          // perform the rotation that will actually decrease black height
+          this.rightRotate(grandparent);
+          parent.color = NodeColor.BLACK; // rotation made parent become the new subtree root...
+          grandparent.color = NodeColor.RED; // the former grandparent is a sibling of node...
+          break; // ...subtree (parent) is black, so no need to iterate more!
+        }
+      } else {
+        // everything in this block is a miror of above
+        const uncle = grandparent.left;
+
+        if (uncle?.color === NodeColor.RED) {
+          uncle.color = NodeColor.BLACK;
+          parent.color = NodeColor.BLACK;
+          grandparent.color = NodeColor.RED;
+          node = grandparent;
+        } else {
+          if (node === parent.left) {
+            this.rightRotate(parent);
+            node = parent;
+            parent = node.parent!;
+          }
+          this.leftRotate(grandparent);
+          parent.color = NodeColor.BLACK;
+          grandparent.color = NodeColor.RED;
+          break; // Fix done
+        }
+      }
+    }
   }
 
   delete(value: T): void {
@@ -101,47 +177,53 @@ export class RedBlackTree<T> {
   }
 
   private leftRotate(node: Node<T>): void {
-    if (node.right === null) return;
-    const newRoot = node.right; // refers to this subtree root
+    if (node.right === null) return; // subtree's future root must exist
+    const newRoot = node.right;
 
-    if (this.root === node) this.root = newRoot; // actual root pointer
-
-    // actual rotation
-    const leftSubtree = newRoot.left;
-    newRoot.left = node;
-
-    // parents reassignments
-    const nodeParent = node.parent;
-    node.parent = newRoot;
-
-    newRoot.parent = nodeParent;
-    if (nodeParent?.right === node) nodeParent.right = newRoot;
-    else if (nodeParent?.left === node) nodeParent.left = newRoot;
-
-    node.right = leftSubtree;
+    // save former left subtree, if exists
+    node.right = newRoot.left;
     if (node.right) node.right.parent = node;
+
+    // handle parent pointers
+    newRoot.parent = node.parent; // 1. for new root parent
+    if (newRoot.parent === null) {
+      this.root = newRoot; // 2. for tree root
+    } else if (node === newRoot.parent.left) {
+      // 3a. for parent's child (left case)
+      newRoot.parent.left = newRoot;
+    } else {
+      // 3b. for paren'ts child (right case)
+      newRoot.parent.right = newRoot;
+    }
+
+    // finally, the "actual" rotation
+    newRoot.left = node;
+    node.parent = newRoot;
   }
 
   private rightRotate(node: Node<T>): void {
     if (node.left === null) return;
-    const newRoot = node.left; // refers to this subtree root
+    const newRoot = node.left;
 
-    if (this.root === node) this.root = newRoot; // actual root pointer
-
-    // actual rotation
-    const rightSubtree = newRoot.right;
-    newRoot.right = node;
-
-    // parents reassignments
-    const nodeParent = node.parent;
-    node.parent = newRoot;
-
-    newRoot.parent = nodeParent;
-    if (nodeParent?.right === node) nodeParent.right = newRoot;
-    else if (nodeParent?.left === node) nodeParent.left = newRoot;
-
-    node.left = rightSubtree;
+    // save former right subtree, if exists
+    node.left = newRoot.right;
     if (node.left) node.left.parent = node;
+
+    // handle parent pointers
+    newRoot.parent = node.parent; // 1. for new root parent
+    if (newRoot.parent === null) {
+      this.root = newRoot; // 2. for tree root
+    } else if (node === newRoot.parent.left) {
+      // 3a. for parent's child (left case)
+      newRoot.parent.left = newRoot;
+    } else {
+      // 3b. for paren'ts child (right case)
+      newRoot.parent.right = newRoot;
+    }
+
+    // finally, the "actual" rotation
+    newRoot.right = node;
+    node.parent = newRoot;
   }
 
   private getMin(node: Node<T>): Node<T> {
